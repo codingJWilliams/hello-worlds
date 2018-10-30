596b077301eec5b185c71a150070bea1ffd3c89a
Jay Williams <codingJWilliams@users.noreply.github.com>
2018-10-30 13:39:21 +0000

    Update Python_9.py

--- examples/p/Python/Python_9.py
+++ examples/p/Python/Python_9.py
@@ -6,10 +6,10 @@ fig.suptitle('HelloWorld_Plot', fontsize=14, fontweight='bold')
 fig.subplots_adjust(top=0.85)
 
 ax = fig.add_subplot(111)
-ax.set_xlabel('hello')
-ax.set_ylabel('world')
+ax.set_xlabel('Hello')
+ax.set_ylabel('World')
 
-ax.text(4, 5, 'hello world', style='italic',
+ax.text(4, 5, 'Hello "World', style='italic',
         bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
 ax.axis([0, 10, 0, 10])
 
